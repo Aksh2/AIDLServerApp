4b74158e2531c1107129164e8f581b37e2535ed0
Akshay Kumar H <akshaykumarh94@gmail.com>
2025-06-23 06:16:08 +0530

    Code refactor for encryption.

--- app/src/main/java/com/project/applicationb/SecureEncryptionService.kt
+++ app/src/main/java/com/project/applicationb/SecureEncryptionService.kt
@@ -2,8 +2,10 @@ package com.project.applicationb
 
 import android.app.Service
 import android.content.Intent
+import android.content.ServiceConnection
 import android.os.IBinder
 import android.util.Log
+import com.project.applicationb.util.CryptoHelper.decrypt
 
 
 class SecureEncryptionService : Service() {
@@ -12,8 +14,26 @@ class SecureEncryptionService : Service() {
     }
 
     private val mBinder = object : IEncryptionService.Stub() {
+
         override fun sendOneWayMessage(message: String?) {
-            Log.d(TAG, "OneWay Message Received: $message")
+            Log.d(
+                TAG,
+                "OneWay Message Received: $message."
+            )
+            try {
+                val decryptedMessage = message?.decrypt()
+                Log.d(TAG, "decrypted Message Received 1: $decryptedMessage")
+            }catch (
+                e: Exception
+            ) {
+                Log.d(TAG, "decrypted Message Received 1: ${e.printStackTrace()}")
+            }
+
+
+            /*if(message.isNullOrBlank().not()){
+                Log.d(TAG, "decrypted Message Received 2: $decryptedMessage")
+
+            }*/
         }
 
         override fun twoWayMessaging(message: String?): String {
@@ -37,10 +57,14 @@ class SecureEncryptionService : Service() {
         super.onDestroy()
     }
 
+    override fun unbindService(conn: ServiceConnection) {
+        super.unbindService(conn)
+    }
+
     override fun onBind(intent: Intent): IBinder {
         Log.d(
             TAG,
-            "Binding service in thread id: ${Thread.currentThread().id} and name: ${Thread.currentThread().name} pid: ${android.os.Process.myPid()}"
+            "Binding service intent $intent in thread id: ${Thread.currentThread().id} and name: ${Thread.currentThread().name} pid: ${android.os.Process.myPid()}"
         )
         return mBinder
     }
